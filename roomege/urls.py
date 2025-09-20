2646912e2ffe441c3d4f96275d53f5638edf1f72
Aaron Cumming <aaroncumming04@gmail.com>
2025-09-20 11:12:48 -0500

    fixed url

--- roomege/urls.py
+++ roomege/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = "roomege"
 urlpatterns = [
         path("profile/<int:pk>", views.ProfileView.as_view(), name="profile"),
-        path("create_profile/", views.CreateProfileView.as_view(), name="create_profile"),
+        path("create_profile/", views.CreateUserView.as_view(), name="create_profile"),
 ]
