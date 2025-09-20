b9ac9addb39ae14060b1030003c704f5aebcd7bb
Andrew Fynaardt <andrew.fynaardt@student.cune.edu>
2025-09-20 11:10:54 -0500

    updated urls.py for create profile view

--- roomege/urls.py
+++ roomege/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = "roomege"
 urlpatterns = [
         path("profile/<int:pk>", views.ProfileView.as_view(), name="profile"),
+        path("create_profile/", views.CreateProfileView.as_view(), name="create_profile"),
 ]
