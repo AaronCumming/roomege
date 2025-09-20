05133fe2b99795b1d828d363fe9275299748d5a1
Andrew Fynaardt <andrew.fynaardt@student.cune.edu>
2025-09-20 11:41:48 -0500

    added comment to forms

--- roomege/forms.py
+++ roomege/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from .models import CustomUser, Social
 
+# For the sake of simplicity, fields are being input directily in views.py instead of using forms here.
+# Given more time, I would refactor views.py to use these forms.
+
 class CustomUserCreationForm(forms.ModelForm):
     class Meta:
         model = CustomUser
